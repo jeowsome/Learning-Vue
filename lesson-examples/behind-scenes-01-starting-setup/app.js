8d63184150c010642b09db2af9f5123fd97db2c0
jeowsome <jeowsome15@gmail.com>
2022-10-27 00:26:11 +0800

    Learning about Refs and Vue LifeCycle Hooks

---
+++ lesson-examples/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,61 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userInput.value;
+    },
+  },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  created() {
+    console.log('created');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  unmounted() {
+    console.log('unmounted');
+  },
+});
+
+app.mount('#app');
+app.unmount();
+
+// Learning about Proxies
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello World!',
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!';
+    }
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
